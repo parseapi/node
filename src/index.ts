f5bb8a19d62830f962fbeca8dab8383f39309c73
Billy O'Shall <billy@oshall.com>
2026-09-02 09:17:59 -0400

    Release 0.2.1.

--- src/index.ts
+++ src/index.ts
@@ -47,7 +47,7 @@ import type {
 
 export * from './types.js';
 
-const VERSION = '0.2.0';
+const VERSION = '0.2.1';
 const DEFAULT_BASE_URL = 'https://api.parseapi.com';
 const DEFAULT_TIMEOUT_MS = 10_000;
 const DEFAULT_RETRIES = 2;
